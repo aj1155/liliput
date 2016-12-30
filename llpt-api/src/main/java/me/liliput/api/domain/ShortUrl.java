d26252a2faf004bdf94bf0574d6113c34ac25fd3
eenan <eenan@sk.com>
2016-12-30 18:33:37 +0900

    Short URL 관련 entity, repository 추가

--- llpt-api/src/main/java/me/liliput/api/domain/ShortUrl.java
+++ llpt-api/src/main/java/me/liliput/api/domain/ShortUrl.java
@@ -1,8 +1,61 @@
 package me.liliput.api.domain;
 
+import javax.persistence.*;
+import java.io.Serializable;
+
 /**
  * Created by 1002731 on 2016. 12. 30..
  * Email : eenan@sk.com
  */
-public class ShortUrl {
+@Entity
+public class ShortUrl extends BaseEntity implements Serializable{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String domain;
+    private String path;
+    private String originUrl;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDomain() {
+        return domain;
+    }
+
+    public void setDomain(String domain) {
+        this.domain = domain;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public String getOriginUrl() {
+        return originUrl;
+    }
+
+    public void setOriginUrl(String originUrl) {
+        this.originUrl = originUrl;
+    }
+
+    @Override
+    public String toString() {
+        return "ShortUrl{" +
+                "id=" + id +
+                ", domain='" + domain + '\'' +
+                ", path='" + path + '\'' +
+                ", originUrl='" + originUrl + '\'' +
+                '}';
+    }
 }
--- llpt-api/src/main/java/me/liliput/api/repository/ShortUrlRepository.java
+++ llpt-api/src/main/java/me/liliput/api/repository/ShortUrlRepository.java
@@ -1,8 +1,11 @@
 package me.liliput.api.repository;
 
+import me.liliput.api.domain.ShortUrl;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 /**
  * Created by 1002731 on 2016. 12. 30..
  * Email : eenan@sk.com
  */
-public class ShortUrlRepository {
+public interface ShortUrlRepository extends JpaRepository<ShortUrl, Long>{
 }
