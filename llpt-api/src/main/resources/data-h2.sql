e4ec40f90170c05644f479e37939b9486afda28e
eenan <eenan@sk.com>
2016-12-31 01:25:29 +0900

    [api] shorten dummy data 추가

--- llpt-api/src/main/resources/data-h2.sql
+++ llpt-api/src/main/resources/data-h2.sql
@@ -1,1 +1,5 @@
 INSERT INTO SHORT_URL(HOST_DOMAIN, PATH, ORIGIN_URL) VALUES ('localhost:8080', 'abcd', 'http://www.naver.com');
+INSERT INTO SHORT_URL(HOST_DOMAIN, PATH, ORIGIN_URL) VALUES ('localhost:8080', 'gWa2', 'http://www.daum.net');
+INSERT INTO SHORT_URL(HOST_DOMAIN, PATH, ORIGIN_URL) VALUES ('localhost:8080', '3PwQ', 'http://www.nate.com');
+INSERT INTO SHORT_URL(HOST_DOMAIN, PATH, ORIGIN_URL) VALUES ('localhost:8080', '5MbK', 'http://www.google.com');
+INSERT INTO SHORT_URL(HOST_DOMAIN, PATH, ORIGIN_URL) VALUES ('localhost:8080', 'dMwN', 'http://www.skpla.net');
