c63b8fa860c1284ebea54ce4fe2889613d06a72e
eenan <eenan@sk.com>
2016-12-30 20:51:31 +0900

    UrlPathHelper 빈 등록

--- llpt-api/src/main/java/me/liliput/api/WebConfig.java
+++ llpt-api/src/main/java/me/liliput/api/WebConfig.java
@@ -1,6 +1,8 @@
 package me.liliput.api;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.util.UrlPathHelper;
 
 /**
  * Created by 1002731 on 2016. 12. 30..
@@ -8,4 +10,9 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 public class WebConfig {
+
+    @Bean
+    public UrlPathHelper urlPathHelper(){
+        return  new UrlPathHelper();
+    }
 }
