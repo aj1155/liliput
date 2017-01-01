19651236ce5e7375a227a2aac3fee7b3b3638e58
eenan <eenan@sk.com>
2017-01-01 11:43:52 +0900

    [dashboard] 개발서버 api 주소 세팅

--- llpt-dashboard/src/main-web/app/shared/constants/index.ts
+++ llpt-dashboard/src/main-web/app/shared/constants/index.ts
@@ -11,7 +11,7 @@ let defaultDashboardApiUrl = DEFAULT_IN_MEMORY_API_URL;
 if (ENV === 'local-server') {
   defaultDashboardApiUrl = 'http://localhost:8080/' + API_PREFIX;
 } else if (ENV === 'dev') {
-  defaultDashboardApiUrl = 'http://localhost:8080/' + API_PREFIX;
+  defaultDashboardApiUrl = 'http://222.239.249.195:8080/list-api/' + API_PREFIX;
 } else if (ENV === 'alpha') {
   defaultDashboardApiUrl = 'http://localhost:8080/' + API_PREFIX;
 } else if (ENV === 'prod') {
