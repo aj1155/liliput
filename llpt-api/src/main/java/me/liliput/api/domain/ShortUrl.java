684898d21025309ed51207e50c0d817fbff03273
eenan <eenan@sk.com>
2017-01-04 22:23:18 +0900

    [api] ShortUrl entity 필드 추가 및 lombok으로 수정

--- llpt-api/src/main/java/me/liliput/api/domain/ShortUrl.java
+++ llpt-api/src/main/java/me/liliput/api/domain/ShortUrl.java
@@ -1,5 +1,9 @@
 package me.liliput.api.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
 import javax.persistence.*;
 import java.io.Serializable;
 
@@ -9,6 +13,9 @@ import java.io.Serializable;
  */
 @Entity
 @Table(name = "SHORT_URL")
+@Getter
+@Setter
+@ToString
 public class ShortUrl extends BaseEntity implements Serializable{
 
     @Id
@@ -21,46 +28,6 @@ public class ShortUrl extends BaseEntity implements Serializable{
     private String path;
     @Column(name = "ORIGIN_URL")
     private String originUrl;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getDomain() {
-        return domain;
-    }
-
-    public void setDomain(String domain) {
-        this.domain = domain;
-    }
-
-    public String getPath() {
-        return path;
-    }
-
-    public void setPath(String path) {
-        this.path = path;
-    }
-
-    public String getOriginUrl() {
-        return originUrl;
-    }
-
-    public void setOriginUrl(String originUrl) {
-        this.originUrl = originUrl;
-    }
-
-    @Override
-    public String toString() {
-        return "ShortUrl{" +
-                "id=" + id +
-                ", domain='" + domain + '\'' +
-                ", path='" + path + '\'' +
-                ", originUrl='" + originUrl + '\'' +
-                '}';
-    }
+    @Column(name = "DESCRIPTION")
+    private String description;
 }
